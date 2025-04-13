72e6b41ef7cb0417f5e76f0e79cd8009ea7eac36
WarrenMartin <martinwarren2000@gmail.com>
2025-04-13 21:23:41 +0530

    brcyptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import UserModel from '../models/user.js';
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
